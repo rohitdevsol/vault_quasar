97fa23eb10188b5f78b25129a499ea0b9b413046
Rohit <rohitnarnolia88@gmail.com>
2026-04-03 19:46:05 +0530

    add: withdraw test

--- src/instructions/withdraw.rs
+++ src/instructions/withdraw.rs
@@ -5,15 +5,20 @@ pub struct Withdraw<'info> {
     pub signer: &'info mut Signer,
     #[account(mut, seeds = [b"vault", signer], bump)]
     pub vault: &'info mut UncheckedAccount,
+    pub system_program: &'info Program<System>,
 }
 
 impl<'info> Withdraw<'info> {
     #[inline(always)]
-    pub fn withdraw(&self, amount: u64) -> Result<(), ProgramError> {
-        let vault = self.vault.to_account_view();
-        let signer = self.signer.to_account_view();
-        set_lamports(vault, vault.lamports() - amount);
-        set_lamports(signer, signer.lamports() + amount);
+    pub fn withdraw(&self, amount: u64, seeds: [Seed<'_>; 3]) -> Result<(), ProgramError> {
+        self.system_program.transfer(self.vault, self.signer, amount).invoke_signed(&seeds)?;
         Ok(())
+
+        // If it was a program owned account we could have done it
+
+        // let vault = self.vault.to_account_view();
+        // let signer = self.signer.to_account_view();
+        // set_lamports(vault, vault.lamports() - amount);
+        // set_lamports(signer, signer.lamports() + amount);
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -5,6 +5,8 @@ use solana_address::Address;
 use quasar_vault_client::DespositInstruction;
 use quasar_vault_client::WithdrawInstruction;
 
+use crate::withdraw;
+
 fn setup() -> QuasarSvm {
     let elf = include_bytes!("../target/deploy/quasar_vault.so");
     QuasarSvm::new().with_program(&Pubkey::from(crate::ID), elf)
@@ -19,6 +21,7 @@ fn test_deposit() {
         &[b"vault", user.as_ref()],
         &crate::ID
     );
+
     let instruction: Instruction = (DespositInstruction {
         signer: Address::from(user.to_bytes()),
         vault,
@@ -28,12 +31,68 @@ fn test_deposit() {
 
     let result = svm.process_instruction(
         &instruction,
-        &[quasar_svm::token::create_keyed_system_account(&user, 10_000_000_000)]
+        &[
+            quasar_svm::token::create_keyed_system_account(&user, 10_000_000_000),
+            quasar_svm::token::create_keyed_system_account(&vault, 0),
+        ]
     );
 
     result.assert_success();
 
-    let vault_after = result.account(&Pubkey::from(vault));
+    let vault_after = result.account(&Pubkey::from(vault.to_bytes())).unwrap();
+    assert_eq!(vault_after.lamports, 1_000_000_000);
+}
 
-    assert!(vault_after.is_none())
+#[test]
+fn test_withdraw() {
+    let mut svm = setup();
+
+    let user = Pubkey::new_unique();
+    let (vault, _vault_bump) = Address::find_program_address(
+        &[b"vault", user.as_ref()],
+        &crate::ID
+    );
+
+    let initial_accounts = vec![
+        quasar_svm::token::create_keyed_system_account(&user, 10_000_000_000),
+        quasar_svm::token::create_keyed_system_account(&vault, 0)
+    ];
+    let deposit_ix: Instruction = (DespositInstruction {
+        signer: Address::from(user.to_bytes()),
+        vault,
+        system_program: Address::from(quasar_svm::system_program::ID.to_bytes()),
+        amount: 1_000_000_000,
+    }).into();
+
+    let dep_results = svm.process_instruction(&deposit_ix, &initial_accounts);
+
+    // let vault_after = dep_results.account(&Pubkey::from(vault)).unwrap();
+    // let user_after = dep_results.account(&Pubkey::from(user)).unwrap();
+    // eprintln!("Vault after deposit: {:#?}", vault_after);
+    // eprintln!("User after deposit: {:#?}", user_after);
+
+    dep_results.assert_success();
+
+    let next_accounts = dep_results.accounts;
+
+    // now let us withdraw from the vault
+    // user and vault is same just use a different instruction
+
+    let withdraw_ix: Instruction = (WithdrawInstruction {
+        amount: 1_000_000_000,
+        signer: user,
+        vault,
+        system_program: Address::from(quasar_svm::system_program::ID.to_bytes()),
+    }).into();
+
+    let withdraw_results = svm.process_instruction(&withdraw_ix, &next_accounts);
+    withdraw_results.assert_success();
+
+    let vault_after1 = withdraw_results.account(&Pubkey::from(vault)).unwrap();
+    let user_after1 = withdraw_results.account(&Pubkey::from(user)).unwrap();
+    // the balance in the vault should be 0
+    eprintln!("Vault1 after deposit: {:#?}", vault_after1);
+    eprintln!("User1 after deposit: {:#?}", user_after1);
+    assert_eq!(vault_after1.lamports, 0);
+    assert_eq!(user_after1.lamports, 10_000_000_000);
 }
